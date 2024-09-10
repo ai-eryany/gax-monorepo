2d474a55a84e5fc88e90580975f71f7a0396589c
Panda <102668388+aiman99aleryany@users.noreply.github.com>
2024-09-10 23:12:46 +0300

    DELETE: moved the files to the styles dir

--- apps/gax-app/src/interface/components/styles/skill-card.comp.module.scss.d.ts
+++
@@ -1,9 +0,0 @@
-declare const classNames: {
-    readonly flipBox: 'flipBox';
-    readonly flipBoxInner: 'flipBoxInner';
-    readonly txtBox: 'txtBox';
-    readonly flipBoxFront: 'flipBoxFront';
-    readonly flipBoxBack: 'flipBoxBack';
-    readonly imgBox: 'imgBox';
-};
-export = classNames;
