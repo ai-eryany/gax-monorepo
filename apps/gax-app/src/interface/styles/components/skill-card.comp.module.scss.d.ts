a5c3911d9f2ee70c28f99986e958146777769147
Panda <102668388+aiman99aleryany@users.noreply.github.com>
2024-09-10 23:46:32 +0300

    UPDATE: Skill-card is done

--- apps/gax-app/src/interface/styles/components/skill-card.comp.module.scss.d.ts
+++ apps/gax-app/src/interface/styles/components/skill-card.comp.module.scss.d.ts
@@ -1,7 +1,9 @@
 declare const classNames: {
     readonly flipBox: 'flipBox';
     readonly flipBoxInner: 'flipBoxInner';
+    readonly frontMask: 'frontMask';
     readonly txtBox: 'txtBox';
+    readonly descBox: 'descBox';
     readonly flipBoxFront: 'flipBoxFront';
     readonly flipBoxBack: 'flipBoxBack';
     readonly imgBox: 'imgBox';
